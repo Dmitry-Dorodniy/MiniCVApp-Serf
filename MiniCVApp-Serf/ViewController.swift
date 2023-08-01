f2d1c07793c1b25d7f2fcfae315d26756bc4e139
Dmitry-Dorodniy <dmitrydorodniy@gmail.com>
2023-08-01 13:09:55 +0300

    Initial Commit

---
+++ MiniCVApp-Serf/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniCVApp-Serf
+//
+//  Created by Dmitry Dorodniy on 01.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
